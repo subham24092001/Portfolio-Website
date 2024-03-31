3cc4a9985f00a43a0dfff567e2f9268785c49723
Subh <singhsubham62810@gmail.com>
2024-03-31 17:02:13 +0530

    Modified

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,21 +10,29 @@ const Portfolio = () => {
       id: 1,
       src: Ecommerce,
       href:"https://65faa402f697514481a023b2--admirable-platypus-d57ee8.netlify.app/",
+      front:"https://github.com/subham24092001/Mernecomefrontend",
+      back:"https://github.com/subham24092001/Mernecomebackend",
     },
     {
       id: 2,
       src: FileSharing,
-      href:"https://6609382a079846a068d9c6d1--dreamy-capybara-b5ac55.netlify.app/"
+      href:"https://6609382a079846a068d9c6d1--dreamy-capybara-b5ac55.netlify.app/",
+      front:"https://github.com/subham24092001/filesharingfrontend",
+      back:"https://github.com/subham24092001/filesharingbackend",
     },
     {
       id: 2,
       src: blog,
       href:"https://blogprj-f12ef.web.app",
+      front:"https://github.com/subham24092001/blogwebsite",
+      back:null
     },
     {
       id: 3,
       src: chatapp,
       href:"https://chatapp-88e0e.web.app",
+      front:"https://github.com/subham24092001/ChatApp",
+      back:null
     },
   ];
 
@@ -44,7 +52,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 py-6 px-12 sm:px-0">
-          {portfolio.map(({ id, src,href }) => {
+          {portfolio.map(({ id, src,href ,front,back}) => {
             return (
               <div key={id} className="shadow-md shadow-gray-500 rounded-lg">
                 <img
@@ -53,14 +61,26 @@ const Portfolio = () => {
                   className="rounded-md duration-200 hover:scale-105"
                 />
                 <div className="flex flex-col items-center justify-center">
+                  <div className="flex items-center justify-evenly">
+                 
                   <a href={href} target="_subh">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <button className="w-1/2 px-6 py-2 m-2 duration-200 hover:scale-105">
                     Demo
                   </button>
                   </a>
-                  {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Code
-                  </button> */}
+                  <div className="flex flex-col items-center justify-center">
+                  <a href={front} target="_subh">
+                  <button className="w-1/2 py-2 duration-200 hover:scale-105">
+                    Frontend Code
+                  </button>
+                  </a>
+                  <a href={back} target="_subh">
+                  <button className="w-1/2 py-2 duration-200 hover:scale-105">
+                    backend Code
+                  </button>
+                  </a>
+                  </div>
+                  </div>
                 </div>
               </div>
             );
